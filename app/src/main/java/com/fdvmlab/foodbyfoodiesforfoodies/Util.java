3018dce0989e52c68169c7736e002bd9628fdaca
Flavio Moreira <mr.flaviomoreira@gmail.com>
2020-12-03 23:49:23 +0000

    remove upload method

--- app/src/main/java/com/fdvmlab/foodbyfoodiesforfoodies/Util.java
+++ app/src/main/java/com/fdvmlab/foodbyfoodiesforfoodies/Util.java
@@ -1,17 +1,6 @@
 package com.fdvmlab.foodbyfoodiesforfoodies;
 
-import android.graphics.Bitmap;
-import android.util.Log;
 import android.util.Patterns;
-
-import androidx.annotation.NonNull;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-
-import java.io.ByteArrayOutputStream;
 
 public class Util {
     /**
@@ -31,7 +20,7 @@ public class Util {
      * @return true if is valid or false otherwise
      */
     public static boolean isPasswordValid(String password) {
-        return password.length() > 6;
+        return password.length() >= 6;
     }
 
     /**
@@ -40,33 +29,7 @@ public class Util {
      * @param text plain text
      * @return true if is valid or false otherwise
      */
-    public static boolean isTextValid(String text) {
+    public static boolean isTextInvalid(String text) {
         return text.isEmpty();
-    }
-
-    /**
-     * Upload the bitmap
-     *
-     * @param bitmapImage
-     * @param imageId
-     * @param storageReference
-     */
-    public void uploadImage(Bitmap bitmapImage, String imageId, StorageReference storageReference) {
-        // convert to bytes
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-
-        // move bitmap obj into baos
-        bitmapImage.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-
-        // upload image
-        storageReference.child(imageId).putBytes(baos.toByteArray())
-                .addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
-                        if (task.isSuccessful()) {
-                            Log.d("", "Upload" + ((task.isSuccessful()) ? "successful!" : " failed!"));
-                        }
-                    }
-                });
     }
 }
