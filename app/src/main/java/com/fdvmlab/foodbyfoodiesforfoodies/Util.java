68fdf36aa1262bdf1dab9d6e0426ebf5033e1d11
Flavio Moreira <mr.flaviomoreira@gmail.com>
2020-12-03 05:01:41 +0000

    implement upload image method

--- app/src/main/java/com/fdvmlab/foodbyfoodiesforfoodies/Util.java
+++ app/src/main/java/com/fdvmlab/foodbyfoodiesforfoodies/Util.java
@@ -1,12 +1,23 @@
 package com.fdvmlab.foodbyfoodiesforfoodies;
 
+import android.graphics.Bitmap;
+import android.util.Log;
 import android.util.Patterns;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
 
 public class Util {
     /**
      * Validate email address
      *
-     * @param emailAddress
+     * @param emailAddress email address
      * @return true if is valid or false otherwise
      */
     public static boolean isEmailValid(String emailAddress) {
@@ -16,7 +27,7 @@ public class Util {
     /**
      * validate password
      *
-     * @param password
+     * @param password firebase app password
      * @return true if is valid or false otherwise
      */
     public static boolean isPasswordValid(String password) {
@@ -26,10 +37,36 @@ public class Util {
     /**
      * validate input text
      *
-     * @param text
-     * @return
+     * @param text plain text
+     * @return true if is valid or false otherwise
      */
     public static boolean isTextValid(String text) {
         return text.isEmpty();
+    }
+
+    /**
+     * Upload the bitmap
+     *
+     * @param bitmapImage
+     * @param imageId
+     * @param storageReference
+     */
+    public void uploadImage(Bitmap bitmapImage, String imageId, StorageReference storageReference) {
+        // convert to bytes
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        // move bitmap obj into baos
+        bitmapImage.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+
+        // upload image
+        storageReference.child(imageId).putBytes(baos.toByteArray())
+                .addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("", "Upload" + ((task.isSuccessful()) ? "successful!" : " failed!"));
+                        }
+                    }
+                });
     }
 }
