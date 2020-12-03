8926dd6c12fc592b92af7c2513119e46e7041dab
Flavio Moreira <mr.flaviomoreira@gmail.com>
2020-12-03 03:14:56 +0000

    implement password and plain text validation

--- app/src/main/java/com/fdvmlab/foodbyfoodiesforfoodies/Util.java
+++ app/src/main/java/com/fdvmlab/foodbyfoodiesforfoodies/Util.java
@@ -12,4 +12,24 @@ public class Util {
     public static boolean isEmailValid(String emailAddress) {
         return Patterns.EMAIL_ADDRESS.matcher(emailAddress).matches();
     }
+
+    /**
+     * validate password
+     *
+     * @param password
+     * @return true if is valid or false otherwise
+     */
+    public static boolean isPasswordValid(String password) {
+        return password.length() > 6;
+    }
+
+    /**
+     * validate input text
+     *
+     * @param text
+     * @return
+     */
+    public static boolean isTextValid(String text) {
+        return text.isEmpty();
+    }
 }
