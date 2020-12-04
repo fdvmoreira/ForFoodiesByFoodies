44c87f8e791eafbb935d6541fd4be922d28892dc
Flavio Moreira <mr.flaviomoreira@gmail.com>
2020-12-04 12:19:42 +0000

    setup view holder for food place adapter

--- app/src/main/java/com/fdvmlab/forfoodiesbyfoodies/models/FoodPlaceAdapter.java
+++ app/src/main/java/com/fdvmlab/forfoodiesbyfoodies/models/FoodPlaceAdapter.java
@@ -4,7 +4,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,7 +16,12 @@ import java.util.List;
 
 public class FoodPlaceAdapter extends RecyclerView.Adapter<FoodPlaceAdapter.ViewHolder> {
 
-    List<FoodPlace> foodPlaceList;
+    private List<FoodPlace> foodPlaceList;
+
+    // constructor
+    public FoodPlaceAdapter(List<FoodPlace> foodPlaceList) {
+        this.foodPlaceList = foodPlaceList;
+    }
 
     @NonNull
     @Override
@@ -30,25 +35,31 @@ public class FoodPlaceAdapter extends RecyclerView.Adapter<FoodPlaceAdapter.View
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        holder.ivFoodPlacePhoto.setImageURI(foodPlaceList.get(position).getPhoto());
+        holder.tvFoodPlaceName.setText(foodPlaceList.get(position).getName());
+        holder.tvFoodPlaceCuisine.setText(foodPlaceList.get(position).getName());
+        holder.rFoodPlaceRating.setNumStars((int) foodPlaceList.get(position).getRating().getNumberOfStarts());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return foodPlaceList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        public ImageView imageView;
-        public TextView textView;
-        public RelativeLayout relativeLayout;
+
+        private ImageView ivFoodPlacePhoto;
+        private TextView tvFoodPlaceName, tvReviewNumber, tvFoodPlaceCuisine;
+        private RatingBar rFoodPlaceRating;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            this.imageView = (ImageView) itemView.findViewById(R.id.imageView);
-            this.textView = (TextView) itemView.findViewById(R.id.textView);
-            relativeLayout = (RelativeLayout) itemView.findViewById(R.id.relativeLayout);
+            ivFoodPlacePhoto = itemView.findViewById(R.id.ivCardViewPhoto);
+            tvFoodPlaceName = itemView.findViewById(R.id.tvCardViewRestaurantName);
+            rFoodPlaceRating = itemView.findViewById(R.id.ratingBarCardView);
+            tvReviewNumber = itemView.findViewById(R.id.tvCardViewReviews);
+            tvFoodPlaceCuisine = itemView.findViewById(R.id.tvCardViewCuisine);
         }
     }
 }
