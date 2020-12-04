330720b23bc64fc979fc4a935c0f0eac253577d3
Flavio Moreira <mr.flaviomoreira@gmail.com>
2020-12-04 10:01:38 +0000

    food place list added

---
+++ app/src/main/java/com/fdvmlab/forfoodiesbyfoodies/views/FoodPlaceList.java
@@ -0,0 +1,13 @@
+package com.fdvmlab.forfoodiesbyfoodies.views;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FoodPlaceList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
