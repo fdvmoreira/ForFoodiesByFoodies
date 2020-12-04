fd6ad86792fe2df7a05a9ee9f5a2f7e66abcda0d
Flavio Moreira <mr.flaviomoreira@gmail.com>
2020-12-04 12:16:52 +0000

    add apter to recycle view

--- app/src/main/java/com/fdvmlab/forfoodiesbyfoodies/views/FoodPlaceList.java
+++ app/src/main/java/com/fdvmlab/forfoodiesbyfoodies/views/FoodPlaceList.java
@@ -5,21 +5,31 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.fdvmlab.foodbyfoodiesforfoodies.R;
 import com.fdvmlab.forfoodiesbyfoodies.MainActivity;
+import com.fdvmlab.forfoodiesbyfoodies.models.FoodPlace;
+import com.fdvmlab.forfoodiesbyfoodies.models.FoodPlaceAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FoodPlaceList extends AppCompatActivity {
 
-
+    // type: restaurant or street food stall
     private int foodPlaceType = 0;
+
+    // list
+    List<FoodPlace> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_place_list);
 
-        //get food place
+        //get food place type
         try {
             foodPlaceType = (int) getIntent().getExtras().get(MainActivity.FOOD_PLACE_TYPE);
         } catch (Exception e) {
@@ -30,8 +40,15 @@ public class FoodPlaceList extends AppCompatActivity {
         findViewById(R.id.fbtnActivityFoodPlaceListAddNew).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                //start activity to create new food place and pass type as extra
                 startActivity(new Intent(getApplicationContext(), AddFoodPlace.class).putExtra("FOOD_PLACE_TYPE", foodPlaceType));
             }
         });
+
+        //set adapter
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rvFoodPlaceList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new FoodPlaceAdapter(list));
     }
 }
