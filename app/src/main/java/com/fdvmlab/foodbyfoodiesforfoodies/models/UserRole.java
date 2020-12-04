f108badad72d3a7561b636310c0f3460b5f19746
Flavio Moreira <mr.flaviomoreira@gmail.com>
2020-12-04 02:33:31 +0000

    implement serializable

--- app/src/main/java/com/fdvmlab/foodbyfoodiesforfoodies/models/UserRole.java
+++
@@ -1,5 +0,0 @@
-package com.fdvmlab.foodbyfoodiesforfoodies.models;
-
-public enum UserRole {
-    ADMIN, CRITIC, STANDARD
-}
---
+++ app/src/main/java/com/fdvmlab/forfoodiesbyfoodies/models/UserRole.java
@@ -0,0 +1,7 @@
+package com.fdvmlab.forfoodiesbyfoodies.models;
+
+import java.io.Serializable;
+
+public enum UserRole implements Serializable {
+    ADMIN, CRITIC, STANDARD
+}
