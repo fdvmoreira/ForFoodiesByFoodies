560c97278ce0129c66e005e9aa8d0695ca0b7772
Flavio Moreira <mr.flaviomoreira@gmail.com>
2020-12-04 11:30:53 +0000

    create the view holder

--- app/src/main/java/com/fdvmlab/forfoodiesbyfoodies/models/FoodPlaceAdapter.java
+++ app/src/main/java/com/fdvmlab/forfoodiesbyfoodies/models/FoodPlaceAdapter.java
@@ -1,16 +1,31 @@
 package com.fdvmlab.forfoodiesbyfoodies.models;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.fdvmlab.foodbyfoodiesforfoodies.R;
+
+import java.util.List;
+
 public class FoodPlaceAdapter extends RecyclerView.Adapter<FoodPlaceAdapter.ViewHolder> {
+
+    List<FoodPlace> foodPlaceList;
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        // Inflate the items cards and return it
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.food_place_recycleview_item_cardview, parent, false);
+
+        return new ViewHolder(view);
     }
 
     @Override
@@ -24,8 +39,16 @@ public class FoodPlaceAdapter extends RecyclerView.Adapter<FoodPlaceAdapter.View
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView imageView;
+        public TextView textView;
+        public RelativeLayout relativeLayout;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            this.imageView = (ImageView) itemView.findViewById(R.id.imageView);
+            this.textView = (TextView) itemView.findViewById(R.id.textView);
+            relativeLayout = (RelativeLayout) itemView.findViewById(R.id.relativeLayout);
         }
     }
 }
