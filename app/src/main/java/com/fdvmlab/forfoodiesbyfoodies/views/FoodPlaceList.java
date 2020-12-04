b7eaa8789c705c1e7b07d462b7895c272e135c1b
Flavio Moreira <mr.flaviomoreira@gmail.com>
2020-12-04 10:07:02 +0000

    link view

--- app/src/main/java/com/fdvmlab/forfoodiesbyfoodies/views/FoodPlaceList.java
+++ app/src/main/java/com/fdvmlab/forfoodiesbyfoodies/views/FoodPlaceList.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.fdvmlab.foodbyfoodiesforfoodies.R;
+
 public class FoodPlaceList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_place_list);
     }
 }
