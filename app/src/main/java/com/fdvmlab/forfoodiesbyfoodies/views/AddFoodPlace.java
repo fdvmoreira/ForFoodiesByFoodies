1865e6371f3998b8941d56d9faec32a09ccf1adb
Flavio Moreira <mr.flaviomoreira@gmail.com>
2020-12-04 10:47:44 +0000

    add activity file to create food place

---
+++ app/src/main/java/com/fdvmlab/forfoodiesbyfoodies/views/AddFoodPlace.java
@@ -0,0 +1,40 @@
+package com.fdvmlab.forfoodiesbyfoodies.views;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.fdvmlab.foodbyfoodiesforfoodies.R;
+import com.fdvmlab.forfoodiesbyfoodies.MainActivity;
+
+public class AddFoodPlace extends AppCompatActivity {
+
+    // type: restaurant or street food stall
+    private int foodPlaceType = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_food_place);
+
+        //get food place type
+        try {
+            foodPlaceType = (int) getIntent().getExtras().get(MainActivity.FOOD_PLACE_TYPE);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //save button
+        // init views
+        findViewById(R.id.fbtnActivityFoodPlaceListAddNew).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //start activity to create new food place and pass type as extra
+                startActivity(new Intent(getApplicationContext(), AddFoodPlace.class).putExtra("FOOD_PLACE_TYPE", foodPlaceType));
+            }
+        });
+    }
+}
